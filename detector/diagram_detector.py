74a736d9e6e06889a50139e4f5ffe3f187c1616b
max <->
2017-11-28 11:09:53 +0100

    Add entry point and detector package

---
+++ detector/diagram_detector.py
@@ -0,0 +1,7 @@
+
+class DiagramDetector:
+    def __init__(self):
+        print("DiagramDetector initialized")
+
+    def detect(self):
+        print("Detection started")
---
+++ main.py
@@ -0,0 +1,7 @@
+import cv2
+import imutils
+from detector.diagram_detector import DiagramDetector
+
+if __name__ == '__main__':
+    print("Shape detection started")
+    dd = DiagramDetector()
