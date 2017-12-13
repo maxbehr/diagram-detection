220bfbcbf300930db2cb55dc20ea6d4a0a3fcab2
max <->
2017-12-13 14:17:15 +0100

    Add simple XML writer

---
+++ detector/xml/XMLWriter.py
@@ -0,0 +1,25 @@
+from xml.etree import ElementTree
+from xml.dom import minidom
+
+
+class XMLWriter:
+
+    def __init__(self, output_file):
+        self.file = output_file
+
+    def write(self, elem):
+        """
+        Writes the given shapes to the output file.
+        :return:
+        """
+        file = open(self.file, "w")
+        file.write(self.prettify(elem))
+        file.close()
+
+    def prettify(elem):
+        """
+        Takes the given element and returns it as a prettified XML.
+        """
+        ugly_xml = ElementTree.tostring(elem, 'utf-8')
+        pretty_xml = minidom.parseString(ugly_xml)
+        return pretty_xml.toprettyxml(indent="  ")
