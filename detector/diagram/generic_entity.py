9e6752aa27494b4efe0c5e30a37ad564c7945669
max <->
2018-01-07 17:26:23 +0100

    Add data dict to generic entity

--- detector/diagram/generic_entity.py
+++ detector/diagram/generic_entity.py
@@ -3,7 +3,7 @@ class GenericEntity:
     def __init__(self, shape):
         self.shape = shape
         self.type = None
-        self.class_contour = None
+        self.data = {}
         self.methods = {}
 
     def add_method(self, name, func):
@@ -11,3 +11,9 @@ class GenericEntity:
 
     def call(self, name):
         getattr(self, name)()
+
+    def set(self, name, data):
+        self.data[name] = data
+
+    def get(self, name):
+        return self.data[name]
