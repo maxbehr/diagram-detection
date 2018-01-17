e3e47c501b1e98d6c5a39c017d3ffd3b4111aa93
max <->
2018-01-17 19:27:01 +0100

    Add file to test detection of UML class notation elements

---
+++ image_interaction.py
@@ -0,0 +1,61 @@
+from detector import util
+from detector.detector import *
+from detector.util import log
+
+
+# Press R for Reset
+# Press B for binary image
+# Press I for inverted image
+# Press E for erosion
+# Press D for dilation
+# Press C for Canny Edge
+# Press H for Hough Lines
+# Press P for HoughP Lines
+def image_interaction(ch, image, img_path):
+    # C - Canny Edge
+    if ch == 99:
+        image = util.create_canny_edge_image(image)
+
+    # D - Dilate
+    if ch == 100:
+        log("Dilate")
+        image = util.dilate(image)
+
+    # E - Erode
+    if ch == 101:
+        log("Erode")
+        image = util.erode(image)
+
+    # C - Find Classes
+    if ch == 49:
+        shape_detector = ShapeDetector(img_path)
+        shapes = shape_detector.find_shapes()
+        diagram_converter = DiagramTypeDetector.find_converter(shape_detector)
+        entities = diagram_converter.extract_classes()
+        image = util.draw_class_entities_on_img(entities, image)
+        log(f"{len(shapes)} shapes in image found")
+
+    # S - Extract Shapes
+    if ch == 115:
+        shape_detector = ShapeDetector(img_path)
+        shapes = shape_detector.find_shapes()
+        shape_detector.save_found_shapes()
+        log(f"{len(shapes)} shapes in image found")
+        for s in shapes:
+            util.print_contour_details(s.contour)
+
+    # B - Binary
+    if ch == 98:
+        image = util.create_binary_img(image)
+
+    # I - Inverted
+    if ch == 105:
+        image = util.create_inverted_image(image)
+
+    # R - Reset
+    if ch == 114:
+        shape_detector = ShapeDetector(img_path)
+        image = shape_detector.image
+        original = image.copy()
+
+    return image
---
+++ main_notation_detection.py
@@ -0,0 +1,39 @@
+import argparse
+import cv2
+from detector.detector import *
+from image_interaction import image_interaction
+
+winname = "Window"
+
+winname = "Erosion Dilation"
+image = None
+
+if __name__ == '__main__':
+
+    cv2.namedWindow(winname)
+    cv2.moveWindow(winname, 0, 0)
+
+    img_path = "img/class_diagram_notation.jpeg"
+    #img_path = "img/class_pencil.jpeg"
+    #img_path = "img/class.jpeg"
+    #img_path = "img/class2.jpeg"
+    #img_path = "img/usecase.jpeg"
+    #img_path = "img/circles.jpeg"
+    #img_path = "img/ocr_test.jpeg"
+    #img_path = "img/ocr_test2.jpeg"
+
+    #   Detect all shapes
+    shape_detector = ShapeDetector(img_path)
+    image = shape_detector.image
+    original = image.copy()
+
+    # Loop for get trackbar pos and process it
+    while True:
+        ch = cv2.waitKey(5)
+        image = image_interaction(ch, image, img_path)
+        cv2.imshow(winname, image)
+
+        if ch == 27:
+            break
+
+    cv2.destroyAllWindows()
